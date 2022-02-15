cfda539acfd41eaf509b3246c526f4d3bb6aca96
Bartłomiej Juda <bartekjkk@gmail.com>
2022-02-15 22:11:14 +0100

    jade case kata updated

--- jade_case/jade_case.py
+++ jade_case/jade_case.py
@@ -1,10 +1,9 @@
-def to_jaden_case(string:str):
-    input_str = ''
-    for index,element in enumerate(string.split(" ")):
-        input_str += element.capitalize()
-        if index == [-1]:
-            break
-        else:
-            input_str += " "
-    return input_str[:-1]
+# def to_jaden_case(string:str):
+#     input_str = ''
+#     for index,element in enumerate(string.split(" ")):
+#         input_str += element.capitalize()
+#         input_str += " "
+#     return input_str[:-1]
 
+def to_jaden_case(string):
+    return " ".join(w.capitalize() for w in string.split())
