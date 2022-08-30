e142529cf55e19442e87c9601c3b2e20506f05ac
Bartłomiej Juda <bartekjkk@gmail.com>
2022-08-30 11:40:43 +0200

    is upper

---
+++ is_upper/is_upper.py
@@ -0,0 +1,10 @@
+def is_uppercase(inp):
+#     is_upper = True
+#     for element in inp:
+#         if element.islower() == True:
+#             is_upper = False
+#             break
+#
+#     return is_upper
+
+    return inp == inp.upper()
