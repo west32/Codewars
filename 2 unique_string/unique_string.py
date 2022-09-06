02b5913c43ace3b9f22f8ae7b1a1278325b26e94
Bartłomiej Juda <bartekjkk@gmail.com>
2022-09-06 12:52:19 +0200

    uniqe string

--- 2 unique_string/unique_string.py
+++ 2 unique_string/unique_string.py
@@ -1,17 +1,26 @@
+# def find_uniq(arr):
+#     unique = None
+#     list = []
+#
+#     for x in (arr):
+#         for element in x:
+#             list.append(element)
+#             if list.count(element) == 1:
+#                 unique = element
+#
+#     for element in arr:
+#         if unique in element:
+#             return element
+
+
 def find_uniq(arr):
-    unique = None
-    list = []
+    if set(arr[0].lower()) == set(arr[1].lower()):
+        majority_set = set(arr[0].lower())
+    elif set(arr[0].lower()) == set(arr[2].lower()):
+        majority_set = set(arr[0].lower())
+    else:
+        majority_set = set(arr[1].lower())
 
-    for x in (arr):
-        for element in x:
-            list.append(element)
-            if list.count(element) == 1:
-                unique = element
-
-    for element in arr:
-        if unique in element:
-            return element
-
-
-
-
+    for string in arr:
+        if set(string.lower()) != majority_set:
+            return string
